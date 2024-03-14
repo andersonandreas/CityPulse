dd9cfd3303c259e643f05d28d3b8d5ab2f65ce85
andersonandreas <adde1990@live.se>
2024-03-14 14:51:18 +0100

    add first migration

--- CityPulse.Server/Data/Models/AppDbContext.cs
+++ CityPulse.Server/Data/Models/AppDbContext.cs
@@ -4,6 +4,10 @@ namespace CityPulse.Server.Data.Models
 {
 	public class AppDbContext : DbContext
 	{
+
+		public AppDbContext() : base() { }
+		public AppDbContext(DbContextOptions options) : base(options) { }
+
 		public DbSet<Country> Countries => Set<Country>();
 		public DbSet<City> Cities => Set<City>();
 
--- CityPulse.Server/Program.cs
+++ CityPulse.Server/Program.cs
@@ -1,3 +1,6 @@
+using CityPulse.Server.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opts =>
+{
+	opts.UseSqlServer(builder.Configuration.GetConnectionString("pulseCitiesDb"));
+});
 
 var app = builder.Build();
 
