52ef8cc4233dc3f8dee7fb4dde140da141662503
andersonandreas <adde1990@live.se>
2024-03-14 21:39:34 +0100

    add db seeding controller (read from excel file) and two controllers for cities/countries

---
+++ CityPulse.Server/Controllers/CitiesController.cs
@@ -0,0 +1,100 @@
+﻿using CityPulse.Server.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityPulse.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CitiesController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+
+		public CitiesController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<City>>> GetCities()
+		{
+			return await _context.Cities.ToListAsync();
+		}
+
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<City>> GetCity(int id)
+		{
+			var city = await _context.Cities.FindAsync(id);
+
+			if (city == null)
+			{
+				return NotFound();
+			}
+
+			return city;
+		}
+
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutCity(int id, City city)
+		{
+			if (id != city.Id)
+			{
+				return BadRequest();
+			}
+
+			_context.Entry(city).State = EntityState.Modified;
+
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!CityExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return NoContent();
+		}
+
+
+		[HttpPost]
+		public async Task<ActionResult<City>> PostCity(City city)
+		{
+			_context.Cities.Add(city);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("GetCity", new { id = city.Id }, city);
+		}
+
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteCity(int id)
+		{
+			var city = await _context.Cities.FindAsync(id);
+			if (city == null)
+			{
+				return NotFound();
+			}
+
+			_context.Cities.Remove(city);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		private bool CityExists(int id)
+		{
+			return _context.Cities.Any(e => e.Id == id);
+		}
+	}
+}
---
+++ CityPulse.Server/Controllers/SeedController.cs
@@ -0,0 +1,168 @@
+﻿using CityPulse.Server.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
+using System.Security;
+
+namespace CityPulse.Server.Controllers
+{
+
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class SeedController : ControllerBase
+	{
+
+		private readonly AppDbContext _appDbContext;
+		private readonly IWebHostEnvironment _env;
+
+		public SeedController(AppDbContext appDbContext, IWebHostEnvironment Env)
+		{
+			_appDbContext = appDbContext;
+			_env = Env;
+		}
+
+
+
+
+		[HttpGet]
+		public async Task<ActionResult> Import()
+		{
+			if (!_env.IsDevelopment())
+			{
+				throw new SecurityException("I'm sorry, I can't let you do that.");
+			}
+
+			var path = Path.Combine(_env.ContentRootPath, @"Data\Source\worldcities.xlsx");
+
+			using var stream = System.IO.File.OpenRead(path);
+			using var excelPackage = new ExcelPackage(stream);
+
+			var worksheet = excelPackage.Workbook.Worksheets[0];
+			var nEndRow = worksheet.Dimension.End.Row;
+
+			var numberOfCountriesAdded = 0;
+			var numberOfCitiesAdded = 0;
+
+			var countryByName = _appDbContext.Countries
+				.AsNoTracking()
+				.AsNoTracking()
+				.ToDictionary(c => c.Name, StringComparer.OrdinalIgnoreCase);
+
+
+			// Countries
+			numberOfCountriesAdded = await SeedCountries(
+				worksheet, nEndRow, numberOfCountriesAdded, countryByName);
+
+
+			// Cities 
+			numberOfCitiesAdded = await SeedCities(
+				worksheet, nEndRow, numberOfCitiesAdded, countryByName);
+
+			return new JsonResult(new
+			{
+				cities = numberOfCitiesAdded,
+				country = numberOfCountriesAdded
+			});
+		}
+
+
+
+		private async Task<int> SeedCountries(ExcelWorksheet worksheet, int nEndRow, int numberOfCountriesAdded, Dictionary<string, Country> countryByName)
+		{
+			for (int nRow = 2; nRow <= nEndRow; nRow++)
+			{
+				var row = worksheet.Cells[nRow, 1, nRow, worksheet.Dimension.End.Column];
+
+				var countryName = row[nRow, 5].GetValue<string>();
+				var iso2 = row[nRow, 6].GetValue<string>();
+				var iso3 = row[nRow, 7].GetValue<string>();
+
+				if (countryByName.ContainsKey(countryName))
+				{
+					continue;
+				}
+
+				var country = new Country
+				{
+					Name = countryName,
+					ISO2 = iso2,
+					ISO3 = iso3,
+				};
+
+
+				await _appDbContext.Countries.AddAsync(country);
+				countryByName.Add(countryName, country);
+
+				numberOfCountriesAdded++;
+			}
+
+
+			if (numberOfCountriesAdded > 0)
+			{
+				await _appDbContext.SaveChangesAsync();
+			}
+
+			return numberOfCountriesAdded;
+		}
+
+
+		private async Task<int> SeedCities(
+			ExcelWorksheet worksheet,
+			int nEndRow, int numberOfCitiesAdded,
+			Dictionary<string,
+			Country> countryByName)
+		{
+
+			var cities = _appDbContext.Cities
+				.AsNoTracking()
+				.ToDictionary(c => (
+				Name: c.Name,
+				Lat: c.Lat,
+				Lon: c.Lon,
+				CountryId: c.CountryId
+				));
+
+
+			for (int nRow = 2; nRow <= nEndRow; nRow++)
+			{
+				var row = worksheet.Cells[nRow, 1, nRow, worksheet.Dimension.End.Column];
+
+				var name = row[nRow, 1].GetValue<string>();
+				var lat = row[nRow, 3].GetValue<decimal>();
+				var lon = row[nRow, 4].GetValue<decimal>();
+				var countryName = row[nRow, 5].GetValue<string>();
+
+				var countryId = countryByName[countryName].Id;
+
+				if (cities.ContainsKey((
+					Name: name,
+					Lat: lat,
+					Lon: lon,
+					CountryId: countryId)))
+				{
+					continue;
+				}
+
+
+				var city = new City
+				{
+					Name = name,
+					Lat = lat,
+					Lon = lon,
+					CountryId = countryId
+				};
+
+				await _appDbContext.Cities.AddAsync(city);
+
+				numberOfCitiesAdded++;
+			}
+
+			if (numberOfCitiesAdded > 0)
+			{
+				await _appDbContext.SaveChangesAsync();
+			}
+
+			return numberOfCitiesAdded;
+		}
+	}
+}
